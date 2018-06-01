50f1fbf77deede766aad47bdbd5e9fa140bcd484
yalab <rudeboyjet@gmail.com>
2018-06-01 16:13:56 +0900

    reinterpret_cast

--- ext/unordered_map.cpp
+++ ext/unordered_map.cpp
@@ -1,8 +1,24 @@
 #include "unordered_map.h"
+#include <unordered_map>
 
 void
 Init_unordered_map()
 {
     VALUE rb_cUnorderedMap = rb_define_class("UnorderedMap", rb_cObject);
     rb_include_module(rb_cUnorderedMap, rb_mEnumerable);
+    rb_define_alloc_func(rb_cUnorderedMap, rb_unordered_map_alloc);
+    rb_define_method(rb_cUnorderedMap, "store", reinterpret_cast<VALUE (*)(...)>(rb_unordered_map_aset), 2);
+}
+
+VALUE
+rb_unordered_map_alloc(VALUE obj)
+{
+  std::unordered_map<VALUE, VALUE> map;
+  return obj;
+}
+
+VALUE
+rb_unordered_map_aset(VALUE hash, VALUE key, VALUE val)
+{
+  return hash;
 }
--- ext/unordered_map.h
+++ ext/unordered_map.h
@@ -1,5 +1,7 @@
 #include "ruby.h"
 
 extern "C" {
-  void Init_unordered_map();
+  void Init_unordered_map(void);
+  VALUE rb_unordered_map_alloc(VALUE obj);
+  VALUE rb_unordered_map_aset(VALUE self, VALUE key, VALUE value);
 }
